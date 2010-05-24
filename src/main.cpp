bc71f47aad390730c6a97179024e0b4b61a0e417
Daniel Vaz <danielvaz@gmail.com>
2010-05-24 16:35:08 -0300

    mudan?as necess?rias para realizar o teste de performance

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,13 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     double q[]={1.0, 1.5, 0.0, 0.0, 0.0};
-    double f[]={4.40, 2.3, 0.0, 0.0, 0.0};
+    double f[]={4.40, 2.5, 0.0, 0.0, 0.0};
     //double f[]={2.0, 3.7, 3.13, 0.0, 0.0};
     //Dimensoes para um veiculo
     //double width = 2.5, height = 1.5, body_length = 2.0;
     //Dimensoes para o pioneer 3at
     double width = 0.51, height = 0.493, body_length = 0.26;
-    char logfile[] = "results.log", obstacles_file[]="lasi_map.txt",
-         pathfile[] = "path.log", logcontrol[]="controls.log";
+    char obstacles_file[]="lasi_map.txt", logcontrol[]="controls.log";
     double xcir = 0.008;
     double motor[] = {0.0230, 0.0230, 38.3, 0.71};
     double robot[] = {0.413, 40, 0.043, 0.506, xcir, 0.138, 0.122, 0.1975, 0.4};
@@ -30,12 +29,16 @@ int main(int argc, char *argv[])
     //SkidSteerModel veh(3, xcir);
     CarGeometry geom_car(width, height, body_length);
     World w(obstacles_file, &geom_car);
-    RRT plan(q, f, 3000, &veh, &w, logfile, logcontrol);
-    cvInitSystem(argc, argv);
-    setlocale(LC_NUMERIC, "C");
-    plan.build();
-    plan.close_logfile();
-    plan.path_to_closest_goal();
+    for(int i=0; i<100; i++)
+    {
+        char pathfile[32], logfile[32];
+        snprintf(logfile, 32, "results%d.log", i);
+        RRT plan(q, f, 1000, &veh, &w, logfile, logcontrol);
+        plan.build();
+        plan.close_logfile();
+        snprintf(pathfile, 32, "path%d.log",i);
+        plan.path_to_closest_goal(pathfile);
+    }
     /*Graphics fig(&geom_car);
     fig.plot_obstacles(obstacles_file);
     fig.plot_states(logfile, c_light_green);
--- src/rrt.h
+++ src/rrt.h
@@ -37,7 +37,7 @@ class RRT
         int check_duplicate_node(double *);
         void print_path(const double*, const double*);
         void close_logfile(void);
-        void path_to_closest_goal(void);
+        void path_to_closest_goal(char*);
         ~RRT();
     private:
         Node initial_node, goal_node;
@@ -108,12 +108,13 @@ void RRT::build(void)
         else if(finished == 2)
         {
             cout << "[" << i << "] Objetivo alcancado." << endl;
-            //path_finder();
-            //print_nodes(nodes);
+            //path_to_closest_goal(pathfilename);
             break;
         }
     }
-    cout << "Tentativas de adicionar nos: " << i<< " Previlegiou: " << prev << endl;
+    cout << "Tentativas de adicionar nos: " << j << endl;
+    cout << "Nos adicionados: " << i << endl;
+    cout << "Previlegiou q_goal: " << prev << endl;
 }
 
 int RRT::extend(double *rand)
@@ -238,13 +239,13 @@ void RRT::close_logfile(void)
     fp.close();
 }
 
-void RRT::path_to_closest_goal(void)
+void RRT::path_to_closest_goal(char *filename)
 {
     Dijkstra<Graph, ArcMapWeight> ss(g, (*cost));
     Node closest_goal;
     MyPath p;
     double distance;
-    ofstream pathfile("path.log");
+    ofstream pathfile(filename);
     closest_goal = select_nearest_node(goal_state);
     cout << (*states)[closest_goal][0] << " " << (*states)[closest_goal][1] << " " << (*states)[closest_goal][2] << endl;
     distance = metric((*states)[closest_goal], goal_state);
@@ -258,10 +259,10 @@ void RRT::path_to_closest_goal(void)
     {
         Node s = g.source(a);
         Node t = g.target(a);
-        cout << "From State: " << (*states)[s][0] <<" "<< (*states)[s][1] <<" " << (*states)[s][2] << endl;
+        /*cout << "From State: " << (*states)[s][0] <<" "<< (*states)[s][1] <<" " << (*states)[s][2] << endl;
         cout << "To State: " << (*states)[t][0] <<" "<< (*states)[t][1] <<" " << (*states)[t][2] << endl;
         cout << "Control map: " << (*control_map)[a][0] << " " << (*control_map)[a][1] << endl;
-        cout << "Arc cost: " << (*cost)[a] << endl;
+        cout << "Arc cost: " << (*cost)[a] << endl;*/
         for(int i=0; i<veh->n_states; i++)
             pathfile << (*states)[s][i] << " ";
         pathfile << (*control_map)[a][0] << " " << (*control_map)[a][1] << " "

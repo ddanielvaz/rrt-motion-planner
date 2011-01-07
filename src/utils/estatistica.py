fe40fa13c34580dc4e73bb2b3762eef8adaac718
Daniel Vaz <danielvaz@gmail.com>
2011-01-07 17:55:54 -0200

    calculando nas estatisticas a porcentagem de vezes que o destino foi atingido

--- src/utils/estatistica.py
+++ src/utils/estatistica.py
@@ -11,7 +11,10 @@ avg_node_reached = 0.0
 avg_time_not_reached = 0.0
 avg_total_time = 0.0
 
+count = 0.0
+
 for line in fp:
+    count += 1.0
     line = line.strip()
     pertime,trials,added_nodes,reached = map(float,line.split(";"))
     #print pertime,trials,added_nodes,reached
@@ -31,6 +34,9 @@ avg_node_reached = avg_node_reached / n_reached
 avg_total_time = total_time / (n_reached + not_reached)
 avg_time_not_reached = avg_time_not_reached / not_reached
 
+print "Total experiments:", count
+print "Goal reached %f times." % n_reached
+print "Average of %f percent.", (n_reached/count)*100
 print "Averaged time reached:", avg_time_reached
 print "Averaged trial reached:", avg_trial_reached
 print "Averaged added nodes reached:", avg_node_reached
